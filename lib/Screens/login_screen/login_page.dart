8b118398ba48615ff11eada819bdc872151c4c53
rZoysa <rithikzoysa@gmail.com>
2024-07-04 12:42:36 +0530

    Updated Login Page logic

--- lib/Screens/login_screen/login_page.dart
+++ lib/Screens/login_screen/login_page.dart
@@ -1,8 +1,18 @@
 import 'package:be_glamourous/themes/decoration_helper.dart';
 import 'package:flutter/material.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
+
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final _formKey = GlobalKey<FormState>();
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
+  bool _autoValidate = false;
 
   @override
   Widget build(BuildContext context) {
@@ -26,105 +36,142 @@ class LoginPage extends StatelessWidget {
         },
         child: Container(
           decoration: DecorationHelper.backgroundDecoration(),
-          child: Column(
-            children: [
-              const Spacer(
-                flex: 1,
-              ),
-              SafeArea(
-                child: Padding(
-                  padding: const EdgeInsets.all(24.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        'Email',
-                        style: textFieldLabelStyle,
-                      ),
-                      const TextField(
-                        textInputAction: TextInputAction.next,
-                        decoration: InputDecoration(
-                          hintText: 'Email',
-                          hintStyle: TextStyle(color: Colors.white54),
-                          filled: true,
-                          fillColor: Color.fromRGBO(217, 217, 217, 0.32),
-                          border: border,
-                          enabledBorder: border,
-                          focusedBorder: border,
+          child: Form(
+            key: _formKey,
+            autovalidateMode: _autoValidate
+                ? AutovalidateMode.onUserInteraction
+                : AutovalidateMode.disabled,
+            child: Column(
+              children: [
+                const Spacer(
+                  flex: 1,
+                ),
+                SafeArea(
+                  child: Padding(
+                    padding: const EdgeInsets.all(24.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Email',
+                          style: textFieldLabelStyle,
                         ),
-                        style: TextStyle(color: Colors.white),
-                      ),
-                      const SizedBox(height: 32),
-                      Text(
-                        'Password',
-                        style: textFieldLabelStyle,
-                      ),
-                      const TextField(
-                        textInputAction: TextInputAction.next,
-                        decoration: InputDecoration(
-                          hintText: 'Password',
-                          hintStyle: TextStyle(color: Colors.white54),
-                          filled: true,
-                          fillColor: Color.fromRGBO(217, 217, 217, 0.32),
-                          border: border,
-                          focusedBorder: border,
-                          enabledBorder: border,
+                        TextFormField(
+                          controller: _emailController,
+                          textInputAction: TextInputAction.next,
+                          decoration: const InputDecoration(
+                            hintText: 'Email',
+                            hintStyle: TextStyle(color: Colors.white54),
+                            filled: true,
+                            fillColor: Color.fromRGBO(217, 217, 217, 0.32),
+                            border: border,
+                            enabledBorder: border,
+                            focusedBorder: border,
+                          ),
+                          style: const TextStyle(color: Colors.white),
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return 'Email is required';
+                            }
+                            return null;
+                          },
                         ),
-                        obscureText: true,
-                        style: TextStyle(color: Colors.white),
-                      ),
-                      
-                    ],
+                        const SizedBox(height: 32),
+                        Text(
+                          'Password',
+                          style: textFieldLabelStyle,
+                        ),
+                        TextFormField(
+                          controller: _passwordController,
+                          textInputAction: TextInputAction.done,
+                          decoration: const InputDecoration(
+                            hintText: 'Password',
+                            hintStyle: TextStyle(color: Colors.white54),
+                            filled: true,
+                            fillColor: Color.fromRGBO(217, 217, 217, 0.32),
+                            border: border,
+                            focusedBorder: border,
+                            enabledBorder: border,
+                          ),
+                          obscureText: true,
+                          style: const TextStyle(color: Colors.white),
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return 'Password is required';
+                            }else if(value.length < 8 ){
+                              return 'Password must be at least 8 characters long';
+                            }
+                            return null;
+                          },
+                        ),
+                      ],
+                    ),
                   ),
                 ),
-              ),
-              const Spacer(),
-              Expanded(
-                child: Align(
-                  alignment: Alignment.bottomCenter,
-                  child: Row(
-                    children: [
-                      ElevatedButton(
-                        onPressed: () {
-                          Navigator.pop(context);
-                        },
-                        style: ElevatedButton.styleFrom(
-                          backgroundColor: Colors.transparent,
-                          elevation: 0,
-                        ),
-                        child: Text(
-                          'Back',
-                          style: DecorationHelper.buttonTextStyle(),
-                        ),
-                      ),
-                      Expanded(
-                        child: ElevatedButton(
-                          onPressed: () {},
+                const Spacer(),
+                Expanded(
+                  child: Align(
+                    alignment: Alignment.bottomCenter,
+                    child: Row(
+                      children: [
+                        ElevatedButton(
+                          onPressed: () {
+                            Navigator.pop(context);
+                          },
                           style: ElevatedButton.styleFrom(
-                            backgroundColor:
-                                Theme.of(context).colorScheme.secondary,
+                            backgroundColor: Colors.transparent,
                             elevation: 0,
-                            minimumSize: const Size.square(65),
-                            shape: const RoundedRectangleBorder(
-                              borderRadius: BorderRadius.only(
-                                topLeft: Radius.circular(55),
-                              ),
-                            ),
                           ),
                           child: Text(
-                            'Login',
+                            'Back',
                             style: DecorationHelper.buttonTextStyle(),
                           ),
                         ),
-                      ),
-                    ],
+                        Expanded(
+                          child: ElevatedButton(
+                            onPressed: () {
+                              if (_formKey.currentState?.validate() ?? false) {
+                                // Process login
+                                print("${_emailController.text.toUpperCase().trim()}\n${_passwordController.text}" );
+                              } else {
+                                setState(() {
+                                  _autoValidate = true;
+                                });
+                              }
+                            },
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor:
+                                  Theme.of(context).colorScheme.secondary,
+                              elevation: 0,
+                              minimumSize: const Size.square(65),
+                              shape: const RoundedRectangleBorder(
+                                borderRadius: BorderRadius.only(
+                                  topLeft: Radius.circular(55),
+                                ),
+                              ),
+                            ),
+                            child: Text(
+                              'Login',
+                              style: DecorationHelper.buttonTextStyle(),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
     );
+  }
+
+  @override
+  void dispose() {
+    _emailController.dispose();
+    _passwordController.dispose();
+    super.dispose();
   }
 }
