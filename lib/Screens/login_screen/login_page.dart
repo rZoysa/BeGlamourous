9dc22c54852a58432c1023b3e652c351acc5d5d8
rZoysa <rithikzoysa@gmail.com>
2024-07-08 21:57:44 +0530

    Updated Login functions

--- lib/Screens/login_screen/login_page.dart
+++ lib/Screens/login_screen/login_page.dart
@@ -113,8 +113,6 @@ class _LoginPageState extends State<LoginPage> {
                           validator: (value) {
                             if (value == null || value.isEmpty) {
                               return 'Password is required';
-                            } else if (value.length < 8) {
-                              return 'Password must be at least 8 characters long';
                             }
                             return null;
                           },
