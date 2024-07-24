66495c9f6eebe3d58e511138dde64f34b264401b
rZoysa <rithikzoysa@gmail.com>
2024-07-25 00:37:53 +0530

    removed native splash screen

--- lib/Screens/splash_screen/splash_screen.dart
+++ lib/Screens/splash_screen/splash_screen.dart
@@ -15,24 +15,21 @@ class _SplashScreenState extends State<SplashScreen> {
   }
 
   _navigateToHome() async {
-    await Future.delayed(const Duration(seconds: 3), () {});
-    Navigator.pushReplacementNamed(
-        context, '/home'); // Assuming '/landing' is your landing page route
+    await Future.delayed(const Duration(seconds: 2), () {});
+    Navigator.pushReplacementNamed(context, '/home');
   }
-  
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
+      body: Container(
+        color: const Color.fromARGB(255, 51, 51, 51),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Image.asset('assets/logo.png'), // Replace with your actual logo
+            Image.asset(
+                'assets/images/Logo-Design-Dark.png'), // Replace with your actual logo
             const SizedBox(height: 20),
-            const CircularProgressIndicator(
-              valueColor: AlwaysStoppedAnimation<Color>(Colors.blue),
-            ),
           ],
         ),
       ),
