d3f2311e3c85a371177d306006b4ed6d8eafa9aa
rZoysa <rithikzoysa@gmail.com>
2024-07-25 00:55:06 +0530

    Added custom bottom nav bar

--- lib/Screens/home_screen/home_page.dart
+++ lib/Screens/home_screen/home_page.dart
@@ -1,31 +1,56 @@
+import 'package:be_glamourous/components/custom_nav_bar.dart';
 import 'package:be_glamourous/themes/decoration_helper.dart';
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int _selectedIndex = 0;
+
+  List<Widget> pages = const [];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        decoration: DecorationHelper.backgroundDecoration(),
-        child: Align(
-          alignment: Alignment.bottomCenter,
-          child: BottomNavigationBar(
-            backgroundColor: const Color.fromARGB(255, 173, 173, 173),
-            items: const [
-              BottomNavigationBarItem(
-                icon: Icon(Icons.home),
-                label: '',
+          decoration: DecorationHelper.backgroundDecoration(),
+          child: Stack(
+            children: [
+              SafeArea(
+                child: IndexedStack(
+                  index: _selectedIndex,
+                  children: pages,
+                ),
               ),
-              BottomNavigationBarItem(
-                icon: Icon(Icons.shop),
-                label: '',
+              Align(
+                alignment: Alignment.bottomCenter,
+                child: CurvedNavigationBar(
+                  height: 55,
+                  animationDuration: const Duration(milliseconds: 400),
+                  color: Color.fromARGB(255, 243, 243, 243),
+                  buttonBackgroundColor: const Color(0xFFD1A55C),
+                  animationCurve: Curves.ease,
+                  backgroundColor: Colors.transparent,
+                  items: const <Widget>[
+                    Icon(Icons.home, size: 30, color: Colors.black),
+                    Icon(Icons.shopping_cart, size: 30, color: Colors.black),
+                    // Icon(Icons.compare_arrows, size: 30, color: Colors.black),
+                    // Icon(Icons.settings, size: 30, color: Colors.black),
+                  ],
+                  onTap: (value) {
+                    setState(() {
+                      _selectedIndex = value;
+                    });
+                  },
+                ),
               ),
             ],
-          ),
-        ),
-      ),
+          )),
     );
   }
 }
