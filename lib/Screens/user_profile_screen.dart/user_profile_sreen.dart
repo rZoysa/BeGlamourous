a2e14e82975df7f57e771826f6ac4f36618311fa
rZoysa <rithikzoysa@gmail.com>
2024-07-27 23:32:34 +0530

    UI Update

--- lib/Screens/user_profile_screen.dart/user_profile_sreen.dart
+++ lib/Screens/user_profile_screen.dart/user_profile_sreen.dart
@@ -9,13 +9,102 @@ class UserProfileSreen extends StatefulWidget {
 }
 
 class _UserProfileSreenState extends State<UserProfileSreen> {
+  final List<String> buttonText = const [
+    'Your Details',
+    'Your analyzes',
+    'App settings',
+    'Support',
+    'Terms and conditions'
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       backgroundColor: Colors.transparent,
       body: Column(
         children: [
-          UserProfileAppBar()          
+          const UserProfileAppBar(),
+          const SizedBox(height: 50),
+          ListView.builder(
+            shrinkWrap: true,
+            itemCount: buttonText.length,
+            itemBuilder: (context, index) {
+              return Column(
+                children: [
+                  ElevatedButton(
+                    style: ButtonStyle(
+                      backgroundColor: WidgetStateProperty.all(
+                        const Color.fromARGB(76, 153, 153, 153),
+                      ),
+                      minimumSize: WidgetStateProperty.all(
+                        const Size(double.infinity, 50),
+                      ),
+                      elevation: const WidgetStatePropertyAll(0),
+                      shape: const WidgetStatePropertyAll(
+                        RoundedRectangleBorder(
+                          borderRadius: BorderRadius.all(Radius.zero),
+                        ),
+                      ),
+                    ),
+                    onPressed: () {},
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          buttonText[index],
+                          style: const TextStyle(
+                              color: Colors.white,
+                              fontFamily: 'Jura',
+                              fontSize: 24,
+                              fontWeight: FontWeight.bold),
+                        ),
+                        const Icon(
+                          Icons.arrow_forward_ios,
+                          color: Colors.white,
+                          size: 24,
+                        ),
+                      ],
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 12,
+                  )
+                ],
+              );
+            },
+          ),
+          const Spacer(),
+          ElevatedButton(
+            style: ButtonStyle(
+              backgroundColor: WidgetStateProperty.all(
+                const Color.fromARGB(76, 153, 153, 153),
+              ),
+              minimumSize: WidgetStateProperty.all(
+                const Size(double.infinity, 50),
+              ),
+              elevation: const WidgetStatePropertyAll(0),
+              shape: const WidgetStatePropertyAll(
+                RoundedRectangleBorder(
+                  borderRadius: BorderRadius.all(Radius.zero),
+                ),
+              ),
+            ),
+            onPressed: () {},
+            child: const Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'Logout',
+                  style: TextStyle(
+                      color: Colors.red,
+                      fontFamily: 'Jura',
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold),
+                ),
+              ],
+            ),
+          ),
+          const Spacer(),
         ],
       ),
     );
