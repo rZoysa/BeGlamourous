fcd4885badedb00d919ca07967e1560ba9d80427
rZoysa <rithikzoysa@gmail.com>
2024-07-29 12:59:30 +0530

    Added Skin analyzer page

---
+++ lib/Screens/skin_analyzer_screen/skin_analyzer_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SkinAnalyzerPage extends StatefulWidget {
+  const SkinAnalyzerPage({super.key});
+
+  @override
+  State<SkinAnalyzerPage> createState() => _SkinAnalyzerPageState();
+}
+
+class _SkinAnalyzerPageState extends State<SkinAnalyzerPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
