961c9177205cd0e47e1c903e6d32ebae781d9f66
rZoysa <rithikzoysa@gmail.com>
2024-07-29 13:01:00 +0530

    Added Social media platform

---
+++ lib/Screens/social_platform_screen/social_platform_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SocialPlatformScreen extends StatefulWidget {
+  const SocialPlatformScreen({super.key});
+
+  @override
+  State<SocialPlatformScreen> createState() => _SocialPlatformScreenState();
+}
+
+class _SocialPlatformScreenState extends State<SocialPlatformScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
